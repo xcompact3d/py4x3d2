d01e24900a7bcef84b00434d782d65bfcf851dfc
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2025-06-24 11:14:33 +0100

    Add support for arbitrary orientation and location of cylinder mask

--- tests/test_cylinder.py
+++ tests/test_cylinder.py
@@ -18,32 +18,55 @@ DZ=LZ/(NZ-1)
 
 RCYL=0.5
 
-def get_radius(ijk, dxyz):
-    """Computes the radius of a grid coordinate relative to the origin.
+def get_point_radius(x, origin, axis):
+    """Computes the radius of a point relative to a line given by origin and axis.
+
+    :param x:      The point in space
+    :param origin: The origin vector for the cylinder axis
+    :param axis:   The vector along the cylinder axis
+    :return:       Radius of the point from the origin.
+    """
+
+    # The distance between a point x and a line given by axis a and origin o is given by
+    #    |(x - o) X a| / |a|
+
+    c = np.cross(x - origin, axis)
+    delta = np.linalg.norm(c) / np.linalg.norm(axis)
+    
+    return delta
+    
+def get_grid_point(ijk, dxyz):
+    """Computes the spatial coordinate given a grid reference.
 
     :param ijk:  The grid coordinates (i,j,k)
     :param dxyz: The grid spacing (dx,dy,dz)
-    :return:     Radius of the point from the origin.
+    :return:     The spatial coordinate of the grid reference
     """
-    #
-    # Cartesian coordinates
-    #
-    x = ijk[0] * dxyz[0]
-    y = ijk[1] * dxyz[1]
-    z = ijk[2] * dxyz[2]
-    #
-    # Return the associated radius
-    # FIXME origin and axis of the cylinder can change
-    #
-    return (x**2 + y**2)**0.5
 
-def gencyl(dxyz, n, rcyl):
+    return np.array([ijk[i] * dxyz[i] for i in range(3)])
+
+def get_grid_radius(ijk, dxyz, origin, axis):
+    """Computes the radius of a grid coordinate relative to a line given by origin and axis.
+
+    :param ijk:    The grid coordinates (i,j,k)
+    :param dxyz:   The grid spacing (dx,dy,dz)
+    :param origin: The origin vector for the cylinder axis
+    :param axis:   The vector along the cylinder axis
+    :return:       Radius of the point from the origin.
+    """
+    
+    x = get_grid_point(ijk, dxyz)
+    return get_point_radius(x, origin, axis)
+
+def gencyl(dxyz, n, rcyl, origin, axis):
     """Generates a mask array representing a cylinder.
 
-    :param dxyz: The grid spacing (dx,dy,dz)
-    :param n:    The grid dimensions (nx,ny,nz)
-    :param rcyl: The cylinder radius
-    :return:     Mask array of 0s for exterior points, 1s for interior points.
+    :param dxyz:   The grid spacing (dx,dy,dz)
+    :param n:      The grid dimensions (nx,ny,nz)
+    :param rcyl:   The cylinder radius
+    :param origin: The origin vector for the cylinder axis
+    :param axis:   The vector along the cylinder axis
+    :return:       Mask array of 0s for exterior points, 1s for interior points.
     """
     #
     # Allocate and init to zero
@@ -53,7 +76,7 @@ def gencyl(dxyz, n, rcyl):
     # One when the radius is below rcyl
     #
     for idx, _ in np.ndenumerate(mask):
-        if get_radius(idx, dxyz) <= rcyl:
+        if get_grid_radius(idx, dxyz, origin, axis) <= rcyl:
             mask[idx] = 1.0
     
     return mask
@@ -65,13 +88,13 @@ class TestMask(unittest.TestCase):
     
     def test_x(self):
 
-        mask = gencyl([DX, DY, DZ], [NX, NY, NZ], RCYL)
+        mask = gencyl([DX, DY, DZ], [NX, NY, NZ], RCYL, [0, 0, 0], [0, 0, 1])
         
         for i in range(NX):
             for j in range(NY):
                 for k in range(NZ):
 
-                    r = get_radius([i, j, k], [DX, DY, DZ])
+                    r = get_grid_radius([i, j, k], [DX, DY, DZ], [0, 0, 0], [0, 0, 1])
                     if r > RCYL:
                         self.assertEqual(mask[i][j][k], 0.0)
                     elif r < RCYL:
